d1cdd49e38c97c46ed8e407cb4755f4f14be4721
Khanh Nguyen <nmkhanhbk@gmail.com>
2023-08-30 00:19:04 +0700

    Support multiple binaries

---
+++ src/url_matcher/bin/main.rs
@@ -0,0 +1,50 @@
+pub fn prefix_matches(prefix: &str, request_path: &str) -> bool {
+    let mut request_segs = request_path.split('/');
+    for prefix_seg in prefix.split('/') {
+        let Some(request_seg) = request_segs.next() else {
+            return false;
+        };
+
+        if request_seg != prefix_seg && prefix_seg != "*" {
+            return false;
+        }
+    }
+    return true;
+}
+
+#[test]
+fn test_matches_without_wildcard() {
+    assert!(prefix_matches("/v1/publishers", "/v1/publishers"));
+    assert!(prefix_matches("/v1/publishers", "/v1/publishers/abc-123"));
+    assert!(prefix_matches("/v1/publishers", "/v1/publishers/abc/books"));
+
+    assert!(!prefix_matches("/v1/publishers", "/v1"));
+    assert!(!prefix_matches("/v1/publishers", "/v1/publishersBooks"));
+    assert!(!prefix_matches("/v1/publishers", "/v1/parent/publishers"));
+}
+
+#[test]
+fn test_matches_with_wildcard() {
+    assert!(prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/books"
+    ));
+    assert!(prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/bar/books"
+    ));
+    assert!(prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/books/book1"
+    ));
+
+    assert!(!prefix_matches("/v1/publishers/*/books", "/v1/publishers"));
+    assert!(!prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/booksByAuthor"
+    ));
+}
+
+
+fn main() {
+}
